17747bf90592eff09a6afa3a59196dc68f3b0307
raj-desai <rd9699@gmail.com>
2022-10-09 18:32:49 +0530

    UPDATE: Dish lambda reads url id

--- dish/lambda_function.py
+++ dish/lambda_function.py
@@ -5,8 +5,9 @@ import boto3
 def lambda_handler(event, context):
     # TODO implement
     print("Hello from dish lambda")
+    dish_id = event['pathParameters']['id']
     print(event)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from dish lambda!')
+        'body': json.dumps(f'Hello from {dish_id} dish lambda!')
     }
